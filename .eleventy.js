915c94505b95712ff20fa77fbd122761c60ad5e3
Nirmit P <nirmitpatel16620@gmail.com>
2026-04-03 19:19:56 -0700

    Add eleventy config

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+  // Copy CSS to the output folder
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    templateFormats: ["njk", "html", "md"],
+    htmlTemplateEngine: "njk"
+  };
+};
